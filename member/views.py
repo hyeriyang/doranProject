0fee8f0519bceb45d9a87aa4d7c4e1ff84a23644
mzooh1112 <mzuhui001@gmail.com>
2019-08-02 18:51:08 +0900

    README.md

--- member/views.py
+++ member/views.py
@@ -4,6 +4,9 @@ from django.contrib import auth,messages
 from .models import Profile
 import json
 
+def like(request):
+    return redirect()
+
 def logout(request):
     auth.logout(request)
     return redirect('test')
