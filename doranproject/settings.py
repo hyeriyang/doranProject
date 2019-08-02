8f7f8ce52d1b40d460677c7fadd3d3620120b493
Beakjiyeon <bjy1530@naver.com>
2019-08-02 18:27:44 +0900

    배포 성공git add .git add .git add .git add .git add .git add .git add .git add .!

--- doranproject/settings.py
+++ doranproject/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'xt$vclid&u1x14&+0d(@ach3i8sf4r^v&ye_fuwdu+saa1q*f)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['d-env.btptpy9zja.us-west-2.elasticbeanstalk.com']
-# 'doranproject-env.pmcy4haz4p.us-west-2.elasticbeanstalk.com'
+ALLOWED_HOSTS = []
+# d-env.btptpy9zja.us-west-2.elasticbeanstalk.com
 
 
 # Application definition
